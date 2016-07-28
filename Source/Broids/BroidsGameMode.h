41758d3dc6afe7c7182c7962e9db474d6727f6cb
head2003 <head2003@arcor.de>
2016-07-28 17:23:22 +0200

    Broids Start
    Leeres UE4 Projekt ohne Content

---
+++ Source/Broids/BroidsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BroidsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BROIDS_API ABroidsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
